f66b2abf02b998b230b0e2332b356a4fd7259d9f
Marco <marco.teixeiradasilva@hotmail.fr>
2020-03-16 10:58:30 +0100

    initialisation

--- codebarre.class.php
+++ codebarre.class.php
@@ -4,7 +4,27 @@
 class CodeBarre
 {
 
+public $bandeblanche;
+public $bandenoires;
+public $elementA;
+public $elementB;
+public $elementC;
+public $gardeGauche;
+public $garardeCentrale;
+public $gardeDroite;
+public $numero;
 
-    
+public function __construct($numero) {
+    $this->numero = $numero;
+}
+
+public function getCodeBarre() {
+    return $this->CodeBarre;
+}
+
+public function setCodeBarres($numero) {
+    $this->numero = $numero;
+}
+
 
 }
